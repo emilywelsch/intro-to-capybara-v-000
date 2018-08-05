c8c15b039bf493c4ba50445f81171da64c200103
Emily Welsch <emwelsch@gmail.com>
2018-08-05 12:15:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 class Application < Sinatra::Base
   get '/' do
-    <h1>Welcome!</h1>
-    <h2>If you tell me your name, I can say hi!</h2>
+    # <h1>Welcome!</h1>
+    # <h2>If you tell me your name, I can say hi!</h2>
+    # form goes here
   end
 
 end
